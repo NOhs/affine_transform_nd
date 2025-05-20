a1333d89b7682ed8c4c384cc9e034c5c5dac29f9
NOhs <nicholas.ohs@outlook.com>
2025-05-20 23:01:00 +0200

    Changed setup method to use scikit-build-core.
    Also updated the cmake file for better
    dependency management.

--- affine_transform/__init__.py
+++ affine_transform/__init__.py
@@ -2,7 +2,7 @@ from os import path as _path
 
 from .affine_transform import transform
 
-with open(
-    _path.join(_path.abspath(_path.dirname(__file__)), "version.txt"), encoding="utf-8"
-) as f:
-    __version__ = f.read()
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "0.0.0"
