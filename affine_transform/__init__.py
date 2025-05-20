5b3182a2051ceb7809854ce90713ec5a6582f860
NOhs <nicholas.ohs@outlook.com>
2025-05-21 00:04:39 +0200

    Removed try block for case that should never occur.

--- affine_transform/__init__.py
+++ affine_transform/__init__.py
@@ -2,7 +2,4 @@ from os import path as _path
 
 from .affine_transform import transform
 
-try:
-    from ._version import version as __version__
-except ImportError:
-    __version__ = "0.0.0"
+from ._version import version as __version__
