f04509f5eb29e14cf3423c5abf070a6278897ff2
NOhs <NOhs@users.noreply.github.com>
2025-05-21 00:58:29 +0200

    Reworked and simplified package setup with updated dependencies (#37)

--- affine_transform/__init__.py
+++ affine_transform/__init__.py
@@ -2,7 +2,4 @@ from os import path as _path
 
 from .affine_transform import transform
 
-with open(
-    _path.join(_path.abspath(_path.dirname(__file__)), "version.txt"), encoding="utf-8"
-) as f:
-    __version__ = f.read()
+from ._version import version as __version__
